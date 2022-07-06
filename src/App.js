c6b128309bc15c1770c4e014b16f50497b3f0db6
Aditya Vats <adityavats122017@gmail.com>
2022-07-06 16:47:39 +0530

    Created Shops page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home.component";
 import Wallet from "./pages/wallet/wallet.component";
+import Shops from "./pages/shops/shops.component";
 import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/wallet" element={<Wallet />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/shops" element={<Shops />} />
         <Route exact path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
